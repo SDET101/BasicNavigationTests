ac577ff9a2d3ab5942a307c9fce5af61a04920f9
esalkan <esalkan@icloud.com>
2021-12-25 23:28:49 +0300

    Wikipedia commenting started.

--- src/test/java/com/cbt/tests/WikipediaSearchBox.java
+++ src/test/java/com/cbt/tests/WikipediaSearchBox.java
@@ -10,17 +10,21 @@ public class WikipediaSearchBox {
 
         /*
         Go to wikipedia.org (Links to an external site.)
-enter search term `selenium webdriver`
-click on search button
-click on search result `Selenium (software)`
-verify url ends with `Selenium_(software)'
+        enter search term `selenium webdriver`
+        click on search button
+        click on search result `Selenium (software)`
+        verify url ends with `Selenium_(software)'
          */
 
+        // setup driver
         WebDriver driver = BrowserFactory.getDriver("chrome");
 
+        // get the source url
         driver.get("https://en.wikipedia.org/");
 
+        // find searchbox and send keys
         driver.findElement(By.id("searchInput")).sendKeys("selenium webdriver");
+        // click search button
         driver.findElement(By.id("searchButton")).click();
 
         String urlInWiki = driver.findElement(By.xpath("//a[@title='Selenium (software)']")).getAttribute("href");
